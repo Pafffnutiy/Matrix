02a4dfa0a736aa1f07b4b3ed63695a1fc6b0c3cb
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2020-12-20 16:27:33 +0300

    Add files via upload

---
+++ MyMatrix.hpp
@@ -0,0 +1,39 @@
+#ifndef Matrix_hpp
+#define Matrix_hpp
+
+#include <ostream>
+#include <istream>
+
+struct Matrix
+{
+    size_t rows; //строки
+    size_t cols; //столбцы
+    double **data; //элементы
+    
+    Matrix();//конструктор умолчания
+   ~Matrix();//деструктор
+    Matrix( const Matrix & other );//конструктор копирования
+    Matrix(size_t rows, size_t cols);//конструктор прямоугольной матрицы
+    Matrix(size_t order);//конструктор квалратной матрицы
+
+    void WriteMatrix() const; //вывод в std::cout
+    void SwapRows(size_t r1, size_t r2); //
+    void Triangle();//приведение к треугольному виду
+    long double CalcDeterminant() const;//Вычисление определителя
+    void FillMagickSE();//заполнение матрицы
+    void FillRandomInt();//заполнение случайными целыми
+    void FillRandomDouble();//заполнение случайными double    
+    
+    Matrix AddMatrix( const Matrix & other) const; //создает новую матрицу
+   
+    void ReadMatrix();
+    Matrix operator+ (const Matrix & right) const;
+    Matrix operator- (const Matrix & right) const;
+    
+};
+
+std::ostream & operator<< (std::ostream & left, const Matrix & right);
+std::istream & operator>> (std::istream & in, Matrix & right);
+
+Matrix operator* (double left, const Matrix & right);
+#endif //Matrix_h
